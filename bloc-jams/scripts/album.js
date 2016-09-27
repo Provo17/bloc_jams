9fcd4436efbc2591e4f022a695bc3332aa63d936
Provo17 <provo17.gp@gmail.com>
2016-09-27 17:30:04 -0600

    update

--- bloc-jams/scripts/album.js
+++ bloc-jams/scripts/album.js
@@ -35,7 +35,7 @@ var createSongRow = function(songNumber, songName, songLength) {
         if (currentlyPlayingSongNumber !== songNumber) {
             // Switch from Play -> Pause button to indicate new song is playing.
             $(this).html(pauseButtonTemplate);
-            currentlyPlayingSongNumber = songNumber;
+            setSong(songNumber);
             currentSongFromAlbum = currentAlbum.songs[songNumber - 1]
             updatePlayerBarSong();
         } else if (currentlyPlayingSongNumber === songNumber) {
